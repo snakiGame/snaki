32bec0b880b80c358b627622d42b6d531f898309
Tachera Sasi <tacherasasi@gmail.com>
2025-01-24 15:03:28 +0300

    score db

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,14 @@
-import * as SQLite from 'expo-sqlite';
+import * as SQLite from "expo-sqlite";
 
-export function dbInt(){
-  
+export async function dbInt() {
+  const db = await SQLite.openDatabaseAsync("snaki.db");
+
+  await db.execAsync(`
+  PRAGMA journal_mode = WAL;
+    CREATE TABLE IF NOT EXISTS scores (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    score INTEGER NOT NULL,
+    date TEXT NOT NULL
+  `);
+  return db
 }
