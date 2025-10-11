4b4acddf3a29c5289e268f19f229c173cad6ea49
Tachera Sasi <tacherasasi@gmail.com>
2025-10-11 23:05:12 +0300

    refactor: enhance SplashScreen handling in RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,13 @@
+import { SplashScreen } from "expo-router";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
   return (
     <Stack screenOptions={{
       headerShown:false
