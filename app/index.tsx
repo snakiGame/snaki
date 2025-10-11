be9b3a51a8650fe6557cbdb87e70f369cac716aa
Tachera Sasi <tacherasasi@gmail.com>
2025-10-11 23:09:17 +0300

    refactor: update react-native-safe-area-context dependency to version 5.6.1 and import from correct package

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,6 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
-  SafeAreaView,
   Animated,
   Easing,
   Platform,
@@ -18,6 +17,7 @@ import useSettingStore, { settings_isRondedEdges } from "@/lib/settings";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 import Constants from "expo-constants";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
