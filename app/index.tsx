7c4d005250ec37801c9816643c42e61832c82e82
Tachera Sasi <tacherasasi@gmail.com>
2025-10-14 20:10:52 +0300

    feat: implement router navigation to HomePage after a delay

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,17 @@
 " use dom";
 
-import React from "react";
+import { useRouter } from "expo-router";
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 
 const index = () => {
+  const router = useRouter()
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      router.replace("/home")
+    },2000)
+  },[])
   return (
     <View>
       <Text>Hello Snaki</Text>
